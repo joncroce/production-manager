7a59725e52fd7f86d6a109ee936a6f07de8f1b73
Jon Croce <crocedev@gmail.com>
2023-02-19 08:49:07 -0500

    fix attribute casing

--- src/components/MapEditor/TankIcon.tsx
+++ src/components/MapEditor/TankIcon.tsx
@@ -1,9 +1,9 @@
 export default function TankIcon() {
 	return (<svg width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 		<g>
-			<circle cx="12" cy="12" r="11" fill="#D9D9D9" stroke="black" stroke-width="2" />
-			<line x1="12" y1="20" x2="12" y2="8" stroke="black" stroke-width="2" />
-			<line x1="5" y1="7" x2="19" y2="7" stroke="black" stroke-width="2" />
+			<circle cx="12" cy="12" r="11" fill="#D9D9D9" stroke="black" strokeWidth="2" />
+			<line x1="12" y1="20" x2="12" y2="8" stroke="black" strokeWidth="2" />
+			<line x1="5" y1="7" x2="19" y2="7" stroke="black" strokeWidth="2" />
 		</g>
 	</svg>
 	);
