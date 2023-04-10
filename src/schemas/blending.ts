01c991c8f8a4e0f137c91f1edbb9eb5712ab1b1a
Jon Croce <crocedev@gmail.com>
2023-04-09 21:12:53 -0400

    change: add schema to blending for adding a formula

--- src/schemas/blending.ts
+++ src/schemas/blending.ts
@@ -8,3 +8,30 @@ export const addFormulaSchema = z.object({
 		note: z.string().optional()
 	}))
 });
+
+export type AddFormula = z.infer<typeof addFormulaSchema>;
+
+export const blendComponentSchema = z.object({
+	formulaComponentId: z.string(),
+	blendId: z.string(),
+	sourceTankId: z.string(),
+	targetQuantity: z.coerce.number(),
+	actualQuantity: z.coerce.number().optional(),
+	note: z.string().optional()
+});
+
+export const addBlendSchema = z.object({
+	productId: z.coerce.number().min(100).max(999),
+	formulaId: z.string(),
+	targetQuantity: z.coerce.number(),
+	destinationTankId: z.string(),
+	note: z.string().optional(),
+	components: z.array(
+		z.object({
+			baseCodeId: z.coerce.number().min(100).max(999),
+			sourceTankId: z.string(),
+			targetQuantity: z.coerce.number(),
+			note: z.string().optional()
+		})
+	)
+});
