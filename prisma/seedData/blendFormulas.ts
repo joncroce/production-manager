72b6354b7e16b370c342590861c4a2f869e94d0b
Jon Croce <crocedev@gmail.com>
2023-04-13 22:12:32 -0400

    change: change prisma schema for blends

--- prisma/seedData/blendFormulas.ts
+++ prisma/seedData/blendFormulas.ts
@@ -8,24 +8,35 @@ const {
 	additivePackages
 } = groupBaseCodesByCategory(baseCodes);
 
+const sizeCodeId = 1; // bulk
+const variantCodeId = 0; // unbranded
+
 export const generateRandomBlendFormula = (baseCodeId: number): AddFormula => {
 	const additivePackageFormulaComponent = {
 		baseCodeId: getRandomBaseCodeFromArray(additivePackages).id,
+		sizeCodeId,
+		variantCodeId,
 		proportion: Math.ceil(Math.random() * 5) / 100 // Between 0.01 and 0.05
 	};
 
 	const viscosityImproverFormulaComponent = {
 		baseCodeId: getRandomBaseCodeFromArray(viscosityImprovers).id,
+		sizeCodeId,
+		variantCodeId,
 		proportion: Math.ceil(Math.random() * 20) / 100 // Between 0.1 and 0.20
 	};
 
 	const baseOilFormulaComponent = {
 		baseCodeId: getRandomBaseCodeFromArray(baseOils).id,
+		sizeCodeId,
+		variantCodeId,
 		proportion: 1 - viscosityImproverFormulaComponent.proportion - additivePackageFormulaComponent.proportion
 	};
 
 	return {
 		baseCodeId,
+		sizeCodeId,
+		variantCodeId,
 		formulaComponents: [
 			baseOilFormulaComponent,
 			viscosityImproverFormulaComponent,
