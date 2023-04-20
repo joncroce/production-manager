59bc4fc060a77ef8cfe301631aeac86593e02f84
Jon Croce <crocedev@gmail.com>
2023-04-20 00:38:05 -0400

    refactor UserMenu

--- src/components/UserMenu/index.tsx
+++ src/components/UserMenu/index.tsx
@@ -1,39 +1,37 @@
 import styles from './index.module.css';
-import { signOut } from 'next-auth/react';
-import { useState } from 'react';
 import ProfileIcon from '../Icons/ProfileIcon';
 import ChevronDownIcon from '../Icons/ChevronDownIcon';
-import { Session } from 'next-auth';
-import LoginButton from '../LoginButton';
+import { useState } from 'react';
+import { signOut } from 'next-auth/react';
+import type { User } from 'next-auth';
 
-export default function ({ sessionData }: { sessionData: Session | null; }) {
+const UserMenu: React.FC<{ user?: User; }> = ({ user }) => {
 	const [menuOpen, setMenuOpen] = useState(false);
 
 	function toggleMenu() {
 		setMenuOpen((prev) => !prev);
 	}
 
-	return <div className={styles['user-menu']} data-logged-in={Boolean(sessionData?.user)} data-menu-open={menuOpen}>
-		{sessionData?.user
-			? <>
-				<button className={styles['user-menu__button']} onClick={() => void toggleMenu()}>
-					<div className={styles['user-menu__profile-pic']}>
-						{
-							sessionData.user?.image
-								? <img className={styles['profile-pic__image']} src={sessionData.user.image} />
-								: <ProfileIcon className={styles['profile-pic__placeholder']} />
-						}
-					</div>
-					<span className={styles['user-menu__user-name']}>{sessionData.user?.name ?? 'User'}</span>
+	return user ? (
+		<div className={styles['user-menu']} data-logged-in={Boolean(user)} data-menu-open={menuOpen}>
+			<button className={styles['user-menu__button']} onClick={() => void toggleMenu()}>
+				<div className={styles['user-menu__profile-pic']}>
+					{
+						user?.image
+							? <img className={styles['profile-pic__image']} alt="Profile Pic" src={user.image} />
+							: <ProfileIcon className={styles['profile-pic__placeholder']} />
+					}
+				</div>
+				<span className={styles['user-menu__user-name']}>{user.name}</span>
 
-					<ChevronDownIcon className={styles['user-menu__icon']} />
+				<ChevronDownIcon className={styles['user-menu__icon']} />
 
-				</button>
-				<ul className={styles['user-menu__options']}>
-					<li><button className={styles['user-menu__option']} onClick={() => void signOut()}>Sign Out</button></li>
-				</ul>
-			</>
-			: <LoginButton />
-		}
-	</div>;
-}
+			</button>
+			<ul className={styles['user-menu__options']}>
+				<li><button className={styles['user-menu__option']} onClick={() => void signOut({ redirect: true })}>Sign Out</button></li>
+			</ul>
+		</div>
+	) : null;
+};
+
+export default UserMenu;
