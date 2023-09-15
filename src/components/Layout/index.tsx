6036262083b11072c99372d5ed0b4c2b901d3e9d
Jon Croce <crocedev@gmail.com>
2023-09-14 22:32:26 -0400

    update user nav to use shadcn components

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,18 +1,18 @@
 import React, { type PropsWithChildren } from 'react';
 import { useSession } from 'next-auth/react';
 import MainNav from '@/components/MainNav';
-import UserMenu from '@/components/UserMenu';
+import UserNav from '@/components/UserNav';
 
 const Layout: React.FC<PropsWithChildren> = ({ children }) => {
 	const { data: sessionData } = useSession();
 
 	return (
-		<>
+		<div className="relative flex min-h-screen max-w-6xl mx-auto flex-col">
 			<div className="border-b">
 				<div className="flex h-16 items-center">
 					<MainNav className="mx-6" />
-					<div className="ml-auto flex items-center space-x-4">
-						<UserMenu user={sessionData?.user} />
+					<div className="ml-auto px-2 flex items-center space-x-4">
+						<UserNav user={sessionData?.user} />
 					</div>
 				</div>
 			</div>
@@ -20,7 +20,7 @@ const Layout: React.FC<PropsWithChildren> = ({ children }) => {
 			<main className="flex-1 space-y-4 p-8 pt-6">
 				{children}
 			</main>
-		</>
+		</div>
 	);
 };
 
---
+++ src/components/UserNav/index.tsx
@@ -0,0 +1,52 @@
+import {
+	Avatar,
+	AvatarFallback,
+	AvatarImage,
+} from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuGroup,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import type { User } from 'next-auth';
+
+export default function UserNav({ user }: { user?: User; }) {
+
+	return user ? (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<Button variant="ghost" className="relative h-8 w-8 rounded-full">
+					<Avatar className="h-8 w-8">
+						<AvatarImage src={user.image ?? undefined} alt={`@${user.id}`} />
+						<AvatarFallback>{user.name?.split(' ').map((namePart) => namePart.charAt(0).toUpperCase()).join('')}</AvatarFallback>
+					</Avatar>
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent className="w-56" align="end" forceMount>
+				<DropdownMenuLabel className="font-normal">
+					<div className="flex flex-col space-y-1">
+						<p className="text-sm font-medium leading-none">{user.name}</p>
+						<p className="text-xs leading-none text-muted-foreground">
+							{user.email}
+						</p>
+					</div>
+				</DropdownMenuLabel>
+				<DropdownMenuSeparator />
+				<DropdownMenuGroup>
+					<DropdownMenuItem>
+						Profile
+					</DropdownMenuItem>
+				</DropdownMenuGroup>
+				<DropdownMenuSeparator />
+				<DropdownMenuItem>
+					Log out
+				</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	) : null;
+}
