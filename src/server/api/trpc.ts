162dea3c3010e232ef48ea08dc9f4efab1112ece
Jon Croce <crocedev@gmail.com>
2023-09-18 03:45:32 -0400

    add shared superjson to utils

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -10,7 +10,7 @@
 import { initTRPC, TRPCError } from "@trpc/server";
 import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
 import { type Session } from "next-auth";
-import superjson from "superjson";
+import superjson from '@/utils/superjson';
 import { ZodError } from "zod";
 
 import { getServerAuthSession } from "@/server/auth";
@@ -38,7 +38,7 @@ interface CreateContextOptions {
  *
  * @see https://create.t3.gg/en/usage/trpc#-serverapitrpcts
  */
-const createInnerTRPCContext = (opts: CreateContextOptions) => {
+export const createInnerTRPCContext = (opts: CreateContextOptions) => {
   return {
     session: opts.session,
     prisma,
