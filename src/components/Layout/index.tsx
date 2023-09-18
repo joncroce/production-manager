836cd7c27aa198a71a9c1d5de7f2039ebeaf8314
Jon Croce <crocedev@gmail.com>
2023-09-18 03:46:09 -0400

    add Toaster to Layout component and restyle

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,6 +2,7 @@ import React, { type PropsWithChildren } from 'react';
 import { useSession } from 'next-auth/react';
 import MainNav from '@/components/MainNav';
 import UserNav from '@/components/UserNav';
+import { Toaster } from '@/components/ui/toaster';
 
 const Layout: React.FC<PropsWithChildren> = ({ children }) => {
 	const { data: sessionData } = useSession();
@@ -10,16 +11,17 @@ const Layout: React.FC<PropsWithChildren> = ({ children }) => {
 		<div className="relative flex min-h-screen max-w-6xl mx-auto flex-col">
 			<div className="border-b">
 				<div className="flex h-16 items-center">
-					<MainNav className="mx-6" />
+					<MainNav />
 					<div className="ml-auto px-2 flex items-center space-x-4">
 						<UserNav user={sessionData?.user} />
 					</div>
 				</div>
 			</div>
 
-			<main className="flex-1 space-y-4 p-8 pt-6">
+			<main className="flex-1 space-y-4 mt-6 border rounded-md">
 				{children}
 			</main>
+			<Toaster />
 		</div>
 	);
 };
