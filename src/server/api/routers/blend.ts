6bd5c5f98297b79e1afcac347acd915319c66ffa
Jon Croce <crocedev@gmail.com>
2023-09-29 01:57:15 -0400

    fix handling undefined destination tank when adding blend

--- src/server/api/routers/blend.ts
+++ src/server/api/routers/blend.ts
@@ -74,13 +74,13 @@ export const blendRouter = createTRPCRouter({
 								}
 							}
 						},
-						DestinationTank: {
+						DestinationTank: destinationTankName ? {
 							connect: {
 								name_factoryId: {
 									name: destinationTankName, factoryId
 								}
 							}
-						},
+						} : undefined,
 						targetQuantity,
 						status,
 						note
