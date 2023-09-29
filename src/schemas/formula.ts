aba2cfe29be578f0feb74a7a98bba7bfcd4e86f5
Jon Croce <crocedev@gmail.com>
2023-09-29 00:57:49 -0400

    refine add formula schema

--- src/schemas/formula.ts
+++ src/schemas/formula.ts
@@ -2,15 +2,22 @@ import { z } from 'zod';
 
 export const addFormulaSchema = z.object({
 	factoryId: z.string(),
-	baseCode: z.coerce.number().min(100).max(999),
-	sizeCode: z.coerce.number().optional().default(1),
-	variantCode: z.coerce.number().optional().default(0),
-	formulaComponents: z.array(z.object({
-		baseCode: z.coerce.number().min(100).max(999),
-		sizeCode: z.coerce.number().min(1).optional().default(1),
-		variantCode: z.coerce.number().optional().default(0),
-		proportion: z.coerce.number().min(0).max(1),
-		note: z.string().optional()
-	}))
+	baseCode: z.coerce.number().min(1).max(999),
+	sizeCode: z.coerce.number().min(1),
+	variantCode: z.coerce.number().min(0),
+	formulaComponents: z
+		.array(
+			z.object({
+				baseCode: z.coerce.number().min(100).max(999),
+				sizeCode: z.coerce.number().min(1),
+				variantCode: z.coerce.number().min(0),
+				proportion: z.coerce.number().min(0).max(1),
+				note: z.string().optional()
+			})
+		)
+		.min(2, 'Formula must have at least 2 components.')
+		.refine(components => components.reduce((totalProportion, current) => {
+			return totalProportion + current.proportion;
+		}, 0) === 1, 'Formula component proportions must sum up to 1.')
 });
 export type TAddFormulaSchema = z.infer<typeof addFormulaSchema>;
