c6ce2406877f2a2512af46442c7cbd0345a0e8e7
Jon Croce <crocedev@gmail.com>
2023-10-04 19:53:22 -0400

    remove unused components and pages

--- src/components/Icons/DescendingAlphabeticIcon.tsx
+++
@@ -1,8 +0,0 @@
-const DescendingAlphabeticicIcon: React.FC = ({ }) => (
-	// tabler:sort-descending-letters
-	<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
-		<path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 21v-5c0-1.38.62-2 2-2s2 .62 2 2v5m0-3h-4m4-8h-4l4-7h-4M4 15l3 3l3-3M7 6v12"></path>
-	</svg>
-);
-
-export default DescendingAlphabeticicIcon;
--- src/components/MapEditor/LineIcon.tsx
+++
@@ -1,6 +0,0 @@
-export default function LineIcon() {
-	return (<svg width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-		<line x1="2" y1="12" x2="22" y2="12" stroke="black" stroke-width="3" />
-	</svg>
-	);
-}
